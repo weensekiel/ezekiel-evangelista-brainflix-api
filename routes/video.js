045d0d04093a648fe41fb9d7f92b11ae1714b69b
Ezekiel <evangelista.ezekiel@hotmail.ca>
2024-03-22 19:03:53 -0400

    add post route for new videos and reading/writing from/to json file

--- routes/video.js
+++ routes/video.js
@@ -1,11 +1,32 @@
 import express from "express";
 import { uuid } from "uuidv4";
-import videos from "../data/videos.json" assert {type: "json"};
+import fs from "fs";
 import details from "../data/video-details.json" assert {type: "json"};
 
 const router = express.Router();
 
+router.use("../public/images", express.static("images"));
+router.use(express.json());
+
+function readData() {
+    try {
+        const data = fs.readFileSync("./data/video-details.json");
+        return JSON.parse(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function writeData(data) {
+    try {
+        fs.writeFileSync("./data/video-details.json", JSON.stringify(data, null, 4));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 router.get("/", (req, res) => {
+    const videos = readData();
     res.json(videos);
 })
 
@@ -14,12 +35,26 @@ router.get("/:id", (req, res) => {
 })
 
 router.post("/", (req, res) => {
+    const { title, channel } = req.body;
+
+    if (!title || ! channel) {
+        return res.status(404).send("Title/channel is missing. Please provide a title or channel name");
+    }
+
     const newVideo = {
-        title: "title",
-        channel: "Ezekiel Evangelista",
-        image: "img src",
-        id: uuid()
+        id: uuid(),
+        title,
+        channel,
+        image: "../public/images/cat.png"
     };
+
+    const videos = readData();
+
+    videos.push(newVideo);
+
+    writeData(videos);
+
+    res.status(201).json(videos);
 })
 
 export default router;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import videoRoutes from "./routes/video.js";
 
+dotenv.config();
+
 const app = express();
-const PORT = 5678;
+app.use(cors());
+const { PORT } = process.env;
 
 app.get("/", (req, res) => {
     res.send("this is my swamp!");
